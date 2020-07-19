4a83a60e2198db1e9f3122302297a836c268e230
JuanTrindade <juantrindade123@gmail.com>
2020-07-19 01:58:43 -0300

    ultima att do banco

--- backend/src/database/migrations/20200329153534_category.js
+++ backend/src/database/migrations/20200329153534_category.js
@@ -7,7 +7,7 @@ exports.up = function(knex) {
         table.decimal('telephone').notNullable();
         table.string('city').notNullable();
         table.string('uf', 2).notNullable();
-        table.string('imageCategory');
+        table.string('categoryImage');
     });
 };
 
